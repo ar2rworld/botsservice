defb1e1315f2c692e178c6ed6469732816f610c8
ar2rworld <ar2r.world@gmail.com>
2023-10-31 02:44:17 -0400

    implement basic olabot

---
+++ app/bots/olabot.go
@@ -0,0 +1,26 @@
+package bots
+
+import ms "github.com/ar2rworld/botsservice/app/messageservice"
+
+type OlaBot struct {
+	token string
+}
+
+func (*OlaBot) GetName() string {
+	return "olabot"
+}
+
+func (b *OlaBot) GetToken() string {
+	return b.token
+}
+
+func (*OlaBot) HandleUpdate(u *ms.Update) (ms.MessageReply, error) {
+	if u.Text == "a" {
+		return ms.MessageReply{ Text: "b", ChatID: u.GetChatID(), UserID: u.GetUserID()}, nil
+	}
+	return ms.MessageReply{}, nil
+}
+
+func NewOlaBot(t string) *OlaBot {
+	return &OlaBot{ token: t}
+}
