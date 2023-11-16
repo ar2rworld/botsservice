456d93cfe8b8880018f4bad8215df3df96bcd4f5
ar2rworld <ar2r.world@gmail.com>
2023-11-16 02:20:49 -0500

    add errgroup and coroutines

--- app/client/main.go
+++ app/client/main.go
@@ -28,6 +28,7 @@ import (
 
 	pb "github.com/ar2rworld/botsservice/app/messageservice"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -54,9 +55,9 @@ func main() {
 	defer conn.Close()
 	c := pb.NewMessageServiceClient(conn)
 
-	// Contact the server and print out its response.
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	ctx := context.Background()
+	g, ctx := errgroup.WithContext(ctx)
+
 	t, err := c.Register(ctx, &pb.RegisterRequest{Name: name})
 	if err != nil {
 		log.Fatalf("Could not greet: %v", err)
@@ -70,52 +71,84 @@ func main() {
 		log.Fatal(err)
 	}
 
-	bot.Debug = true
+	bot.Debug = false
 
 	updateConfig := tgbotapi.NewUpdate(0)
 
-	updateConfig.Timeout = 30
-	updates := bot.GetUpdatesChan(updateConfig)
-	
-	go func () {
-		time.Sleep(5 * time.Second)
-		stream, err := c.SendUpdates(ctx, &pb.Updates{ Botname: name })
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = CheckUpdates(stream, bot)
-		if err != nil {
-			log.Println(err)
-		}
-	}()
-
-	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-		log.Printf("Sending update as %s", name)
-
-		u := &pb.Update{
-			ChatID: update.Message.Chat.ID,
-			UserID: update.Message.From.ID,
-			Text: update.Message.Text,
-		}
-
-		stream, err := c.SendUpdates(
-			ctx,
-			&pb.Updates{
-				Botname: name,
-				Updates: []*pb.Update{ u },
-			},
-		)
-		if err != nil {
-			log.Println(err)
-		} else {
-			err = CheckUpdates(stream, bot)
-			if err != nil {
-				log.Println(err)
+	g.Go(func() error {
+		tiker := time.NewTicker(time.Second)
+		
+		for {
+			select {
+				case <- tiker.C:
+					stream, err := c.SendUpdates(ctx, &pb.Updates{ Botname: name })
+					if err != nil {
+						return err
+					}
+					err = CheckUpdates(stream, bot)
+					if err != nil {
+						return err
+					}
+				case <- ctx.Done():
+					return nil
 			}
 		}
+	})
+
+	g.Go(func() error {
+		ticker := time.NewTicker(time.Second)
+
+		for {
+			select {
+				case <- ticker.C:
+					if err != nil {
+						return err
+					}
+
+					updates, err := bot.GetUpdates(updateConfig)
+					if err != nil {
+						return err
+					}
+
+					for _, update := range updates {
+						if update.UpdateID >= updateConfig.Offset {
+							updateConfig.Offset = update.UpdateID + 1
+						}
+
+						if update.Message == nil {
+							continue
+						}
+			
+						u := &pb.Update{
+							ChatID: update.Message.Chat.ID,
+							UserID: update.Message.From.ID,
+							Text: update.Message.Text,
+						}
+			
+						stream, err := c.SendUpdates(
+							ctx,
+							&pb.Updates{
+								Botname: name,
+								Updates: []*pb.Update{ u },
+							},
+						)
+						if err != nil {
+							return err
+						} else {
+							err = CheckUpdates(stream, bot)
+							if err != nil {
+								return err
+							}
+						}
+					}
+				case <- ctx.Done():
+					return nil
+			}
+		}
+	})
+
+	if err := g.Wait(); err != nil {
+		log.Fatalf("Client error: %v", err)
 	}
 }
 
@@ -126,7 +159,7 @@ func CheckUpdates(stream pb.MessageService_SendUpdatesClient, bot *tgbotapi.BotA
 			break
 		}
 		if err != nil {
-			log.Fatalf("SendUpdates(_) = _, %v", err)
+			return err
 		}
 		if mr.GetText() == "" {
 			continue
